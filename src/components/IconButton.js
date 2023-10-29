d552a12cbbd69f71a6acf545ca32e1d0cabe143d
MSI\aasharib <aasharib@onescreensolutions.com>
2023-10-30 00:49:04 +0500

    feat: created custom button component with icon

--- src/components/IconButton.js
+++ src/components/IconButton.js
@@ -2,9 +2,9 @@ import '../assets/stylesheets/IconButton.css'
 
 function IconButton({icon, buttonText, buttonTextColor, backgroundColor}) {
     return (
-        <div className='button button-container' style={{backgroundColor:{backgroundColor}}}>
-            <img src={icon} alt='icon-here' />
-            <p style={{color:{buttonTextColor}}}>{buttonText}</p>
+        <div className='button button-container' style={{backgroundColor:backgroundColor}}>
+            <img className='button-img' src={icon} alt='icon-here' />
+            <p className='button-text' style={{color:buttonTextColor}}>{buttonText}</p>
         </div>
     )
 }
